a0472434c5ba36bdc5cba545917420f711badf70
dxdye <tilman.bertram@campudus.com>
2025-06-30 22:25:12 +0200

    refactor: add main function

--- position/data.py
+++ position/data.py
@@ -1,4 +1,24 @@
+from enum import Enum;
+
+class DataType(Enum):
+    STOCK,
+    CRYPTO,
+    OPTION
+
+class AlpacaAvailablePairs(Enum): 
+    BTCUSD = "BTC/USD",
+    TSLAUSD = "TSLA/USD" #assuming alpaca is usede
+
 
 class Data:
+
+
+    def __init__(self, symbol: AlpacaAvailablePairs, typeOfData: DataType):
+        self.symbol = symbol; 
+        self.typeOfData = typeOfData; 
+    def getFromRemote(self):
+        pass
+    def getFromFile(self):
+        pass
     #add meta data, time frame, begin and end
     pass
