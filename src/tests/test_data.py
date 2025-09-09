d2ecab3215544b24615a0d99bbf477d73e7e9cd7
dxdye <tilman.bertram@campudus.com>
2025-09-10 00:11:16 +0200

    fix: lint

--- src/tests/test_data.py
+++ src/tests/test_data.py
@@ -4,11 +4,12 @@ import data.data as mod
 
 sys.path.append("../")  # appends upper directory
 
+
 def testDataWorkflow():
     """a stub which is not really a test."""
     data0 = mod.Data(
-            symbol=mod.AlpacaAvailablePairs.BTCUSD, timeFrame=mod.TimeFrame.ONEDAY
-            )
+        symbol=mod.AlpacaAvailablePairs.BTCUSD, timeFrame=mod.TimeFrame.ONEDAY
+    )
     url = data0.buildUrl()
     status = data0.fetchFromRemote()
     assert 200 == status
