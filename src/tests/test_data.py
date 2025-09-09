25f910ce3ddff577123f4f148ee700af2503c77d
dxdye <tilman.bertram@campudus.com>
2025-09-10 00:04:02 +0200

    feat: add bdd testing lib pytest_bdd

--- src/tests/test_data.py
+++ src/tests/test_data.py
@@ -1,14 +1,15 @@
 import sys
-
-sys.path.append("../")  # appends upper directory
+from pytest_bdd import when
 import data.data as mod
 
+sys.path.append("../")  # appends upper directory
 
+@when('i get data and compare status code')
 def testDataWorkflow():
     # this is a stub which is not really a test.
     data0 = mod.Data(
-        symbol=mod.AlpacaAvailablePairs.BTCUSD, timeFrame=mod.TimeFrame.ONEDAY
-    )
+            symbol=mod.AlpacaAvailablePairs.BTCUSD, timeFrame=mod.TimeFrame.ONEDAY
+            )
     url = data0.buildUrl()
     status = data0.fetchFromRemote()
     assert 200 == status
