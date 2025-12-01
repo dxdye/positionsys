bde7c51391f1a33c9f05c6f49049a82d60ea3430
dxdye <s-tbertr@haw-landshut.de>
2025-12-01 21:29:12 +0100

    chore: retab

--- src/api/alpaca_crypto_client.py
+++ src/api/alpaca_crypto_client.py
@@ -7,31 +7,27 @@ from src.api.crypto_data import CryptoData
 
 
 class AlpacaCryptoClient:
-    #here we dont need api key and secret, the data is also limited from what ive seen tough
-    def __init__(self):
-        self.client = CryptoHistoricalDataClient()
+  # here we dont need api key and secret, the data is also limited from what ive seen tough
+  def __init__(self):
+    self.client = CryptoHistoricalDataClient()
 
-    def get_crypto_bars(self, symbol: str, start_date: datetime, end_date: datetime):
-        params = CryptoBarsRequest(
-                        symbol_or_symbols=symbol,
-                        timeframe=TimeFrame.Day,
-                        start=start_date,
-                        end=end_date
-                 )
-        try:
-            response = self.client.get_crypto_bars(params)
-            print("succesfully retrieved Crypto data!")
-            bars = response.df.reset_index().to_dict(orient="records")
-            return [CryptoData(**bar) for bar in bars]
-        except Exception as e:
-            print(e)
+  def get_crypto_bars(self, symbol: str, start_date: datetime, end_date: datetime):
+    params = CryptoBarsRequest(symbol_or_symbols=symbol, timeframe=TimeFrame.Day, start=start_date, end=end_date)
+    try:
+      response = self.client.get_crypto_bars(params)
+      print("succesfully retrieved Crypto data!")
+      bars = response.df.reset_index().to_dict(orient="records")
+      return [CryptoData(**bar) for bar in bars]
+    except Exception as e:
+      print(e)
 
-    # not sure if we need more methods/api endpoints?? gotta discuss with colleagues
+  # not sure if we need more methods/api endpoints?? gotta discuss with colleagues
+
 
 if __name__ == "__main__":
-    client = AlpacaCryptoClient()
-    end_date = datetime.datetime.today()
-    start_date = end_date - datetime.timedelta(days=7)
-    bars = client.get_crypto_bars("BTC/USD", start_date, end_date)
-    for bar in bars:
-        print(bar)
+  client = AlpacaCryptoClient()
+  end_date = datetime.datetime.today()
+  start_date = end_date - datetime.timedelta(days=7)
+  bars = client.get_crypto_bars("BTC/USD", start_date, end_date)
+  for bar in bars:
+    print(bar)
--- src/api/crypto_data.py
+++ src/api/crypto_data.py
@@ -2,31 +2,30 @@ import pandas as pd
 
 
 class CryptoData:
-    def __init__(self, **kwargs):
-        self.time = None
-        self.open = None
-        self.high = None
-        self.low = None
-        self.volume = None
-        self.symbol = None
+  def __init__(self, **kwargs):
+    self.time = None
+    self.open = None
+    self.high = None
+    self.low = None
+    self.volume = None
+    self.symbol = None
 
-        self.extract_attributes(**kwargs)
+    self.extract_attributes(**kwargs)
 
-        #right now, we kinda always retrieve all the bars... perhaps we will need smth like only the latest bar? discuss
+    # right now, we kinda always retrieve all the bars... perhaps we will need smth like only the latest bar? discuss
 
-    def extract_attributes(self, **data):
-        self.time = data.get("timestamp")
-        self.open = data.get("open")
-        self.high = data.get("high")
-        self.low = data.get("low")
-        self.volume = data.get("volume")
-        self.symbol = data.get("symbol")
+  def extract_attributes(self, **data):
+    self.time = data.get("timestamp")
+    self.open = data.get("open")
+    self.high = data.get("high")
+    self.low = data.get("low")
+    self.volume = data.get("volume")
+    self.symbol = data.get("symbol")
 
+  def check_if_data_fits_strategy(self):
+    # Here we can check depending on our strategy...
+    # perhaps smth like if tmp is between high and low do smth
+    pass
 
-    def check_if_data_fits_strategy(self):
-        #Here we can check depending on our strategy...
-        # perhaps smth like if tmp is between high and low do smth
-        pass
-
-    def validate_needed_currency(self, symbol: str):
-        return self.symbol == symbol
+  def validate_needed_currency(self, symbol: str):
+    return self.symbol == symbol
