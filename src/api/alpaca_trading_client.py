1fc0c3a102e3dd2cd07d0ae3d15f079d058e36b5
dxdye <s-tbertr@haw-landshut.de>
2025-12-02 05:15:48 +0100

    fix: doc strings

--- src/api/alpaca_trading_client.py
+++ src/api/alpaca_trading_client.py
@@ -2,9 +2,6 @@ import requests
 
 
 class AlpacaTradingClient:
-  BASE_URL = "https://paper-api.alpaca.markets"
-  # we always use paper right now, this class also needs api key and secret
-
   """
   Client to interact with Alpaca Trading API.
   Provides methods to fetch and manage trading positions.
@@ -17,7 +14,20 @@ class AlpacaTradingClient:
   :rtype: None
   """
 
+  BASE_URL = "https://paper-api.alpaca.markets"
+  # we always use paper right now, this class also needs api key and secret
+
   def __init__(self, api_key, api_secret):
+    """
+    Constructor for AlpacaTradingClient class.
+    Initializes the AlpacaTradingClient object with the given API key and secret.
+    :param api_key: Alpaca API key
+    :param api_secret: Alpaca API secret
+    :type api_key: str
+    :type api_secret: str
+    :return: None
+    :rtype: None
+    """
     self.api_key = api_key
     self.api_secret = api_secret
     self.base_url = AlpacaTradingClient.BASE_URL
@@ -27,14 +37,13 @@ class AlpacaTradingClient:
       "accept": "application/json",
     }
 
-  """
-  Fetches all open positions from the Alpaca Trading API.
-  :raises Exception: if there is an error during API request
-  :return: list of open positions
-  :rtype: list
-  """
-
   def get_open_positions(self):
+    """
+    Fetches all open positions from the Alpaca Trading API.
+    :raises Exception: if there is an error during API request
+    :return: list of open positions
+    :rtype: list
+    """
     # https://docs.alpaca.markets/reference/getallopenpositions
     url = self.base_url + "/v2/positions"
     try:
@@ -44,16 +53,15 @@ class AlpacaTradingClient:
     except Exception as e:
       print(e)
 
-  """
-  Fetches a specific position by symbol or asset ID from the Alpaca Trading API.
-  :param symbol_or_asset_id: symbol or asset ID of the position to fetch
-  :type symbol_or_asset_id: str
-  :raises Exception: if there is an error during API request
-  :return: position data
-  :rtype: dict
-  """
-
   def get_specific_position(self, symbol_or_asset_id: str):
+    """
+    Fetches a specific position by symbol or asset ID from the Alpaca Trading API.
+    :param symbol_or_asset_id: symbol or asset ID of the position to fetch
+    :type symbol_or_asset_id: str
+    :raises Exception: if there is an error during API request
+    :return: position data
+    :rtype: dict
+    """
     url = self.base_url + "/v2/positions/" + f"{symbol_or_asset_id}"
     try:
       response = requests.get(url, headers=self.headers)
@@ -62,15 +70,14 @@ class AlpacaTradingClient:
     except Exception as e:
       print(e)
 
-  """
-  Fetches a specific position from all positions by symbol.
-  :param symbol: symbol of the position to fetch
-  :type symbol: str
-  :return: position data if found, else None
-  :rtype: dict or None
-  """
-
   def retrive_specific_pos_from_all_positions(self, symbol=str):
+    """
+    Fetches a specific position from all positions by symbol.
+    :param symbol: symbol of the position to fetch
+    :type symbol: str
+    :return: position data if found, else None
+    :rtype: dict or None
+    """
     positions = self.get_positions()
     for p in positions:
       if p["symbol"] == symbol:
@@ -78,16 +85,15 @@ class AlpacaTradingClient:
     print("no such symbol was found in the positions!")
     return None
 
-  """
-  Closes a specific position by symbol or asset ID using the Alpaca Trading API.
-  :param symbol_or_asset_id: symbol or asset ID of the position to close
-  :type symbol_or_asset_id: str
-  :raises Exception: if there is an error during API request
-  :return: None
-  :rtype: None
-  """
-
   def close_position(self, symbol_or_asset_id: str):
+    """
+    Closes a specific position by symbol or asset ID using the Alpaca Trading API.
+    :param symbol_or_asset_id: symbol or asset ID of the position to close
+    :type symbol_or_asset_id: str
+    :raises Exception: if there is an error during API request
+    :return: None
+    :rtype: None
+    """
     url = self.base_url + "/v2/positions/" + f"{symbol_or_asset_id}"
     try:
       response = requests.delete(url, headers=self.headers)
@@ -97,25 +103,24 @@ class AlpacaTradingClient:
       print(e)
 
   # we probably need orders api points here... i dont know
-  """
-  Places an order using the Alpaca Trading API.
-  :param type: type of the order (e.g., market, limit)
-  :param symbol: trading symbol for the order
-  :param time_in_force: time in force for the order (e.g., day,
-    gtc)
-  :param qty: quantity for the order (optional)
-  :param notional: notional value for the order (optional)
-  :type type: str
-  :type symbol: str
-  :type time_in_force: str
-  :type qty: str or None
-  :type notional: str or None
-  :raises Exception: if there is an error during API request
-  :return: None
-  :rtype: None
-  """
-
   def place_order(self, type: str, symbol: str, time_in_force: str, qty: str = None, notional: str = None):
+    """
+    Places an order using the Alpaca Trading API.
+    :param type: type of the order (e.g., market, limit)
+    :param symbol: trading symbol for the order
+    :param time_in_force: time in force for the order (e.g., day,
+      gtc)
+    :param qty: quantity for the order (optional)
+    :param notional: notional value for the order (optional)
+    :type type: str
+    :type symbol: str
+    :type time_in_force: str
+    :type qty: str or None
+    :type notional: str or None
+    :raises Exception: if there is an error during API request
+    :return: None
+    :rtype: None
+    """
     url = self.base_url + "/v2/orders"
     payload = {"type": type, "symbol": symbol, "time_in_force": time_in_force, "qty": qty, "notional": notional}
     try:
